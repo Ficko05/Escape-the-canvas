6865aa94f142109a9b125987c2807b62240c2536
Ficko <you@example.com>
2019-05-14 14:25:35 +0200

    player animation and healthbar

--- Assets/Scripts/characterScript/HealthBarScript.cs
+++ Assets/Scripts/characterScript/HealthBarScript.cs
@@ -1,23 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBarScript : MonoBehaviour {
 
-    //Image healthBar;
+    Image healthBar;
 
     float maxHealth = 100f;
     public static float health;
 
 	
 	void Start () {
-       // healthBar = GetComponent<Image>();
-        health = maxHealth;
+       healthBar = GetComponent<Image>();
+       health = maxHealth;
 		
 	}
 	
 	
 	void Update () {
-     //   healthBar.fillAmount = health / maxHealth;
+        healthBar.fillAmount = health / maxHealth;
 	}
+
+    //add to oncollision  HealthBarScript.health -= 10f;
 }
